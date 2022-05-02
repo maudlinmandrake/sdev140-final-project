15080bb9bf18459ad08d6814e3973b2133547ff3
Jenny Mikac <jennymikac@gmail.com>
2022-05-02 16:45:06 -0400

    Add todos; adjusted size

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ class DigitalOracle(EasyFrame):
     # Press a button and get your fortune told with a tarot deck
     def __init__(self):
         # initialize window and set up components
-        EasyFrame.__init__(self, title = "The Digital Oracle")
+        EasyFrame.__init__(self, title = "The Digital Oracle", width=600, height=800)
         self.setResizable(False)
         imageLabel = self.addLabel(text="", row=0, column=0, sticky="NSEW")
         textLabel = self.addLabel(text="Your fortune is one click away!", row=1, column=0, sticky="NSEW")
-
+        # TODO: Insert button!
+        # display splash image
         self.image = PhotoImage(file="tarot-card.gif")
         imageLabel["image"] = self.image
-
+        # text label to display
         font = Font(family="Times New Roman", size=20, slant="italic")
         textLabel["font"] = font
         textLabel["foreground"] = "purple"
+        #
 
 def main():
+    # Pops up window for app
     DigitalOracle().mainloop()
 
 if __name__ == "__main__":
