8450cbdd01305e94366dd67d15d7ed131b4491e2
Jenny Mikac <jennymikac@gmail.com>
2022-05-11 15:36:54 -0400

    Don't need get methods, not editing the deck

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class DigitalOracle(EasyFrame):
     # Press a button and get your fortune told with a tarot deck
     def __init__(self):
         # initialize window and set up components
-        EasyFrame.__init__(self, title = "The Digital Oracle", width=600, height=800)
+        EasyFrame.__init__(self, title="The Digital Oracle", width=600, height=800)
         self.setResizable(False)
         imageLabel = self.addLabel(text="", row=0, column=0, sticky="NSEW")
         textLabel = self.addLabel(text="Your fortune is one click away!", row=1, column=0, sticky="NSEW")
@@ -19,6 +19,10 @@ class DigitalOracle(EasyFrame):
         font = Font(family="Times New Roman", size=20, slant="italic")
         textLabel["font"] = font
         textLabel["foreground"] = "purple"
+
+    def get_fortune(self):
+        pass
+
 
 
 def main():
